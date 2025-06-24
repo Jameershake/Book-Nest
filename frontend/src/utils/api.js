4d59937834780898f62e67ef7d8a70bd90927280
Jameer <jameershaik667@gmail.com>
2025-06-24 16:51:07 +0530

    Updated project files

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE,
+  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE
 
-  headers: { 'Content-Type': 'application/json' },
+  //headers: { 'Content-Type': 'application/json' },
 });
 
 api.interceptors.request.use(config => {
