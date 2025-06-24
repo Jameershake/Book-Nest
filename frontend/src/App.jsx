5f6a9345c32b173e92426b95abd48d901e457d5c
Jameer <jameershaik667@gmail.com>
2025-06-24 15:56:48 +0530

    Fix: set start script to use server.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import HomePage from './pages/HomePage.jsx';
