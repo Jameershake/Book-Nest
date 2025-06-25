7b360cb498038f7313bc65f361f8c76ab793c3bb
Jameer <jameershaik667@gmail.com>
2025-06-25 14:30:51 +0530

    Add Stripe payment success page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,8 +30,8 @@ export default function App() {
           <Route path="/orders" element={<OrderHistoryPage
            />} />
            <Route path="/checkout" element={<CheckoutPage />} />
-           <Route path="/success" element={<Success />} />
-           <Route path="/cancel" element={<Cancel />} />
+           {/* <Route path="/success" element={<Success />} />
+           <Route path="/cancel" element={<Cancel />} /> */}
            {/* <Route path="/check" element={<CheckoutButton />} /> */}
 
 
--- frontend/src/components/Success.jsx
+++ frontend/src/components/Success.jsx
@@ -1,4 +1,4 @@
-// components/Success.jsx
-export default function Success() {
-  return <h2>✅ Payment Successful</h2>;
-}
+// // components/Success.jsx
+// export default function Success() {
+//   return <h2>✅ Payment Successful</h2>;
+// }
