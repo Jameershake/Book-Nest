0640fff3cadb1610543282af038009e1ca0d2a78
Jameer <jameershaik667@gmail.com>
2025-06-25 14:22:46 +0530

    Add Stripe payment success page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import SignupPage from './pages/SignupPage.jsx';
 import OrderHistoryPage from './pages/OrderHistoryPage.jsx';
 import CheckoutPage from './pages/CheckoutPage.jsx';
 import Success from './components/Success';
-import Success from './pages/CheckoutButton.jsx';
+//import Success from './pages/CheckoutButton.jsx';
 import Cancel from './pages/Cancel';
 //import CheckoutButton from './pages/CheckoutButton.jsx';
 
